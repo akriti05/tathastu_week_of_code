640b845f068696658d8610bbdabba28bd7d601a8
akriti05 <64738274+akriti05@users.noreply.github.com>
2020-05-28 23:27:08 +0530

    Create prog3.py

---
+++ day3/prog3.py
@@ -0,0 +1,9 @@
+def dupli(string):
+    duplicateString = ""
+    for x in string:
+        if x not in duplicateString:
+            duplicateString += x
+    return duplicateString
+
+string = input("Enter the string: ")
+print("String after removing the duplicates is:", dupli(string))
